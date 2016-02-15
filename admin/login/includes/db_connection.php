e7e1cffa1790e36440b53a12868cd98ad0c45a73
Alaa E. Abdelfattah <aabdelfa@icloud.com>
2016-02-15 14:47:28 -0500

    Update db_connection.php

--- admin/login/includes/db_connection.php
+++ admin/login/includes/db_connection.php
@@ -1,7 +1,7 @@
 <?php
   define("DBSERVER", "localhost");
-  define("DB_USER", "aabdelfa");
-  define("DB_PASS", "alaaisagr8guy");
+  define("DB_USER", "USERNAME");
+  define("DB_PASS", "PASSWORD");
   define("DB_NAME", "ezzy-statistics");
   // 1. Create a database connection
   $connection = mysqli_connect(DBSERVER, DB_USER, DB_PASS, DB_NAME);
